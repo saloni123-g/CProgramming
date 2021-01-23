064f0ea1361f58b7972bfc72e8183d892f122240
saloni123-g <77579620+saloni123-g@users.noreply.github.com>
2021-01-23 20:43:27 +0530

    Update loop.c
    c program to display 6 stair binary number using loop

--- loop.c
+++ loop.c
@@ -4,19 +4,19 @@ int main()
 {
     int i, j, n;
 
-    /* Input number of rows from user */
+    
     printf("Enter value of n: ");
     scanf("%d", &n);
 
     for(i=1; i<=n; i++)
     {
-        /* Print i number of stars */
+       
         for(j=1; j<=i; j++)
         {
             printf("01");
         }
 
-        /* Move to next line */
+       
         printf("\n");
     }
 
